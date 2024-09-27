dadcc381e84355ebfcc2b88abbf57f0d8bc0b684
ftxdante <blog.anutrof@gmail.com>
2024-09-26 21:43:17 -0300

    implementando rotas CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,68 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+from starlette.responses import HTMLResponse
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     """Return a message to root path"""
 
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_html():
+    """Return a html message to html path"""
+
+    return """<title>Hello World</title>
+                <h1>Hello World! </h1>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema) -> UserPublic:
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+    return UserPublic(**user_with_id.model_dump())
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def get_users():
+    # return UserList(users=database)
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return UserPublic(**user_with_id.model_dump())
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
+)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
